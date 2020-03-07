a8ae9f4f3bf26087f48401fdcd5ebe9852e0f7fc
goseeya <149542@gmail.com>
2020-03-07 17:32:27 +0100

    Use context api

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
 import { toggleFav } from '../../store/actions/products';
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
   };
 
-  return (
+  return ( 
     <Card style={{ marginBottom: '1rem' }}>
       <div className="product-item">
         <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
